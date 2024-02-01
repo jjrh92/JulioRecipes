73f5e579ad7a48e5b3b085b53e1e91c0247e55a2
Julio Reyes <juliojesusreyes@gmail.com>
2024-02-01 17:46:57 -0300

    Initial commit

--- components/Layout.js
+++ components/Layout.js
@@ -5,13 +5,11 @@ export default function Layout({ children }) {
     <div className="layout">
       <header>
         <Link href="/">
-          <a>
             <h1>
-              <span>Just Add</span>
-              <span>Marmite</span>
+              <span>Julio</span>
+              <span>Recetas</span>
             </h1>
             <h2>Spread The Joy</h2>
-          </a>
         </Link>
       </header>
 
@@ -20,7 +18,7 @@ export default function Layout({ children }) {
       </div>
 
       <footer>
-        <p>Copyright 2021 Just Add Marmite :)</p>
+        <p>2024 - Julio Reyes</p>
       </footer>
     </div>
   )
